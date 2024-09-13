f522e2623f102ebba0115ec5ee3620ef52380400
cboxrieder <cboxrieder@yahoo.com>
2024-09-13 11:16:28 -0400

    fix build_image/dockerfile to include DTP host. increasing threads for cmake build to 24

--- .devcontainer/build_image.sh
+++ .devcontainer/build_image.sh
@@ -4,6 +4,8 @@ PARASOFT_CPPTEST_STD_FOLDER="/opt/parasoft/cpptest/std/2024.1.0"
 PARASOFT_CPPTEST_CT_FOLDER="/opt/parasoft/cpptest/ct/2024.1.0"
 PARASOFT_SE_GP=$(echo $(<~/.github/pat.txt))
 GITHUB_AGENT_VERSION=2.319.1
+DTP_SERVER_HOSTNAME="ox-server"
+DTP_SERVER_IP="192.168.2.149"
 
 regex=".* ([0-9]{4}\.[0-9]{1}\.[0-9]{1}) \(([0-9\.]+)B([0-9]+)\).*([0-9]{4}).*"
 
@@ -28,4 +30,4 @@ if [[ ! -f "./${CPPTEST_CT_PACKAGE}" ]]; then
   tar -C ${PARASOFT_CPPTEST_CT_FOLDER} -czvf "./${CPPTEST_CT_PACKAGE}" .
 fi
 
-docker build --build-arg CPPTEST_STD="${CPPTEST_STD_PACKAGE}" --build-arg CPPTEST_CT="${CPPTEST_CT_PACKAGE}" --build-arg GITHUB_PAT="${PARASOFT_SE_GP}" --build-arg GITHUB_AGENT_VERSION="${GITHUB_AGENT_VERSION}" -t parasoft_iceoryx_demo .
+docker build --build-arg CPPTEST_STD="${CPPTEST_STD_PACKAGE}" --build-arg CPPTEST_CT="${CPPTEST_CT_PACKAGE}" --build-arg GITHUB_PAT="${PARASOFT_SE_GP}" --build-arg GITHUB_AGENT_VERSION="${GITHUB_AGENT_VERSION}" --add-host=${DTP_SERVER_HOSTNAME}:${DTP_SERVER_IP} -t parasoft_iceoryx_demo .
