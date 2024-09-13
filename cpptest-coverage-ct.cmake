4a48a4c27f8a0f929c90e4f8aa4b0f7685acb02f
cboxrieder <cboxrieder@yahoo.com>
2024-09-13 14:48:59 -0400

    coverage fix #4

--- cpptest-coverage-ct.cmake
+++ cpptest-coverage-ct.cmake
@@ -93,10 +93,10 @@ function (cpptest_enable_coverage)
       -compiler ${CPPTEST_COMPILER_ID}
       ${CPPTEST_COVERAGE_TYPE_INSTRUMENTATION}
       -exclude "regex:*"
-      -include "regex:${CPPTEST_SOURCE_DIR}/../iceoryx_hoofs/container/include/iox/*"
+      #-include "regex:${CPPTEST_SOURCE_DIR}/../iceoryx_hoofs/container/include/iox/*"
       -include "regex:${CPPTEST_SOURCE_DIR}/../iceoryx_hoofs/test/moduletests/test_container_vector.cpp"
       -exclude "regex:${CPPTEST_BINARY_DIR}/*"
-      -ignore "regex:*test_container_vector.cpp"
+      #-ignore "regex:*test_container_vector.cpp"
       -ignore "regex:${CPPTEST_BINARY_DIR}/*"
       )
 
