bb2563c2696fb0fbb9ad9c747794c35fdfec712a
cboxrieder <cboxrieder@yahoo.com>
2024-09-13 10:26:33 -0400

    updates

--- .devcontainer/build_image.sh
+++ .devcontainer/build_image.sh
@@ -2,7 +2,7 @@
 
 PARASOFT_CPPTEST_STD_FOLDER="/opt/parasoft/cpptest/std/2024.1.0"
 PARASOFT_CPPTEST_CT_FOLDER="/opt/parasoft/cpptest/ct/2024.1.0"
-PARASOFT_SE_GITHUB_PAT=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
+PARASOFT_SE_GITHUB_PAT=11BCQ7ODQ09CQmrzDMk6HB_Aso24nNHz3O36rAPGImnHzvCyy8y7C5zjpGecLNnYmyGMWC4XMKMT2cIYG6
 GITHUB_AGENT_VERSION=2.319.1
 
 regex=".* ([0-9]{4}\.[0-9]{1}\.[0-9]{1}) \(([0-9\.]+)B([0-9]+)\).*([0-9]{4}).*"
@@ -28,4 +28,4 @@ if [[ ! -f "./${CPPTEST_CT_PACKAGE}" ]]; then
   tar -C ${PARASOFT_CPPTEST_CT_FOLDER} -czvf "./${CPPTEST_CT_PACKAGE}" .
 fi
 
-docker build --build-arg CPPTEST_STD="${CPPTEST_STD_PACKAGE}" --build-arg CPPTEST_CT="${CPPTEST_CT_PACKAGE}" --build-arg GITHUB_PAT="${PARASOFT_SE_GITHUB_PAT}" --build-arg GITHUB_AGENT_VERSION="${GITHUB_AGENT_VERSION}" -t parasoft_iceoryx_demo .
+docker build --build-arg CPPTEST_STD="${CPPTEST_STD_PACKAGE}" --build-arg CPPTEST_CT="${CPPTEST_CT_PACKAGE}" --build-arg GITHUB_PAT="github_pat_${PARASOFT_SE_GITHUB_PAT}" --build-arg GITHUB_AGENT_VERSION="${GITHUB_AGENT_VERSION}" -t parasoft_iceoryx_demo .
